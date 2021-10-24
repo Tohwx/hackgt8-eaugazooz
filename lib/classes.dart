f67329738a9f5271c5b7fceccc822583ac49576b
amwnag <amwang36@gmail.com>
2021-10-24 00:52:24 -0400

    WORKING DIVIDERS

--- lib/classes.dart
+++ lib/classes.dart
@@ -3,6 +3,8 @@ import 'package:flutter/material.dart';
 List<Event> items = [
   Event.withParams("Event A", "Yes", true, true, 1.0),
   Event.withParams("Event B", "Yes", true, true, 1.0),
+  Event.nameOnly("Event C"),
+  Event.nameOnly("Event D"),
   Event()
 ];
 
@@ -21,6 +23,8 @@ class Event {
 
   Event();
 
+  Event.nameOnly(this.name);
+
   Event.withParams(
       this.name, this.type, this.hasFood, this.hasSwag, this.progress);
 }
@@ -36,15 +40,23 @@ class EventsTab extends StatefulWidget {
 class _EventsTabState extends State<EventsTab> {
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
-        itemCount: items.length,
-        itemBuilder: (context, index) {
-          return _createTile(items[index]);
-        });
+    return ListView.separated(
+      padding: const EdgeInsets.all(16.0),
+      itemCount: items.length,
+      itemBuilder: (context, index) {
+        return _createTile(items[index]);
+      },
+      separatorBuilder: (context, index) {
+        return const Divider();
+      },
+    );
   }
 
   Widget _createTile(Event e) {
-    return ListTile(title: Text(e.name, style: _biggerFont));
+    return ListTile(
+        title: Text(e.name, style: _biggerFont),
+        // minVerticalPadding: 40.0,
+        leading: const Icon(Icons.account_circle));
   }
 }
 
