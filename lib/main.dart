88edd80e2309fe829e41411afc3205954e13800d
Tohwx <weixiongtoh@gmail.com>
2021-10-23 23:03:53 -0400

    remove top bar

--- lib/main.dart
+++ lib/main.dart
@@ -23,12 +23,36 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class TabBarDemo extends StatelessWidget {
-  const TabBarDemo({Key? key}) : super(key: key);
+class TabBarMenu extends StatelessWidget {
+  const TabBarMenu({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return DefaultTabController(
+      length: 3,
+      child: Scaffold(
+        appBar: AppBar(
+          bottom: const TabBar(
+            indicatorColor: Colors.amber,
+            tabs: [
+              Tab(icon: Icon(Icons.home)),
+              Tab(icon: Icon(Icons.map)),
+              Tab(icon: Icon(Icons.leaderboard)),
+            ],
+          ),
+          title: const Text('Opportuno'),
+        ),
+        body: const TabBarView(
+          children: [
+            Icon(Icons.home),
+            Icon(Icons.map),
+            Icon(Icons.leaderboard),
+          ],
+        ),
+      ),
+    );
+
+    /*return MaterialApp(
       home: DefaultTabController(
         length: 3,
         child: Scaffold(
@@ -51,7 +75,7 @@ class TabBarDemo extends StatelessWidget {
           ),
         ),
       ),
-    );
+    );*/
   }
 }
 
@@ -95,45 +119,6 @@ class _MyHomePageState extends State<MyHomePage> {
     // The Flutter framework has been optimized to make rerunning build methods
     // fast, so that you can just rebuild anything that needs updating rather
     // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Container(
-              child: Text(''),
-              margin: EdgeInsets.only(left: 25 * 2, right: 5),
-            ),
-            Expanded(child: TabBarDemo())
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+    return TabBarMenu();
   }
 }
